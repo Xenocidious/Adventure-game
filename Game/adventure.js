92a722ab1a6b6c9adb9af8f778e83ac0ccd89a70
Xeno <Xenocidechan@gmail.com>
2018-12-04 14:48:09 +0100

    Finished
    This is a finished version of the game. (there are no known bugs/mistakes)

--- Game/adventure.js
+++ Game/adventure.js
@@ -5,7 +5,7 @@ var button3 = document.getElementById('button3');
 var desc = document.getElementById('description');
 var title = document.getElementById('title');
 var invItem = document.getElementById('inventoryItem');
-var inventoryItem ={"crowbar":false,"explosives":false,"scroll":false,"key":false,"statue":false};
+var inventoryItem ={"crowbar":false,"explosives":false,"scroll":false,"key":false,"statue":false,"invStatue":false,"deadend2skip":false};
 
 button1.innerHTML = "";
 button2.innerHTML = "";
@@ -14,18 +14,24 @@ button4.innerHTML = "";
 
 title.innerHTML = "";
 desc.style.display = "none";
+invItem.style.display = "none";
 button1.style.transition = "0.3s";
+button2.style.transition = "0.3s";
+button3.style.transition = "0.3s";
+button4.style.transition = "0.3s";
 
 start();
 function start() {
     container.style.backgroundImage = "url('image/startscreen.jpg')";
     title.innerHTML = "";
-
+    container.style.marginTop = "-12px";
+   
+    title.style.width = "565px";
     button1.style.display = "";
     button2.style.display = "none";
     button3.style.display = "none";
     button4.style.display = "none";
-    invItem.style.display = "none";
+    button1.innerHTML = "";
     button1.style.backgroundImage = "url('image/start.png')";
     button1.style.backgroundSize = "225px 75px"
     button1.style.width = "225px";
@@ -38,6 +44,7 @@ function start() {
 function lvl1() {
     container.style.backgroundImage = "url('image/entrance.jpg')";
     title.innerHTML = "You came across the entrance of a tomb.";
+    container.style.marginTop = "5px";
 
     title.style.width = "565px";
     button1.style.marginTop = "0";
@@ -51,7 +58,6 @@ function lvl1() {
     button1.innerHTML = "Go inside";
     button2.innerHTML = "Leave";
     invItem.style.display = "none";
-    button1.style.borderRadius = "50px";
 
     button1.onclick = lvl2;
     button2.onclick = lvl0;
@@ -249,13 +255,18 @@ function lvl4v2() {
     invItem.style.width = "234px";
 
     button1.onclick = lvl3;
-    invItem.onclick = functExplosives;
 
     if (inventoryItem['scroll'] == true) {
         button1.onclick = lvl3noItem;
     }
     else if (inventoryItem['scroll'] == false) {
         button1.onclick = lvl3;
+    }
+    if (inventoryItem['statue'] == true) {
+        invItem.onclick = carryStatue;
+    }
+    else if (inventoryItem['statue'] == false) {
+        invItem.onclick = functExplosives;
     }
 }
 function lvl5() {
@@ -308,9 +319,15 @@ function lvl6() {
     button3.style.width = "100px";
 
     button1.onclick = snakedeath;
-    button2.onclick = lvl7;
     button3.onclick = lvl5;
     button4.onclick = puzzle;
+
+    if (inventoryItem['key'] == true) {
+        button2.onclick = lvl7noItem;
+    }
+    else if (inventoryItem['key'] == false) {
+        button2.onclick = lvl7;
+    }
 }
 function lvl7() {
     container.style.backgroundImage = "url('image/deadend.jpg')";
@@ -335,6 +352,30 @@ function lvl7() {
 
     button1.onclick = lvl6;
     invItem.onclick = functKey;
+
+    if (inventoryItem['explosives'] == true) {
+        button2.onclick = explosiondeath;
+        button2.style.display = "";
+        button2.innerHTML = "Use explosives";
+        button2.style.width = "125px";
+    }
+}
+function lvl7noItem() {
+    container.style.backgroundImage = "url('image/deadend.jpg')";
+    title.innerHTML = "You walked into the dead end again.";
+
+    button1.style.display = "";
+    button2.style.display = "none";
+    button3.style.display = "none";
+    button4.style.display = "none";
+    title.style.width = "535px";
+    button1.innerHTML = "Go back";
+    button2.innerHTML = "Use explosives";
+    button3.innerHTML = "";
+    button4.innerHTML = "";
+    invItem.style.display = "none";
+
+    button1.onclick = lvl6;
 
     if (inventoryItem['explosives'] == true) {
         button2.onclick = explosiondeath;
@@ -397,9 +438,16 @@ function puzzle2() {
     button2.onclick = lvl6;
 
     if (inventoryItem['key'] == true) {
-        button1.onclick = treasure;
-        button1.style.display = "";
-        button1.innerHTML = "Use key";
+        if (inventoryItem['statue'] == true) {
+            button1.onclick = treasurenoItem;
+            button1.style.display = "";
+            button1.innerHTML = "Use key";
+        }
+        else if (inventoryItem['statue'] == false) {
+            button1.onclick = treasure;
+            button1.style.display = "";
+            button1.innerHTML = "Use key";
+        }
     }
     if (inventoryItem['explosives'] == true) {
         button3.onclick = explosiondeath;
@@ -416,7 +464,7 @@ function treasure() {
     button2.style.display = "";
     button3.style.display = "none";
     button4.style.display = "none";
-    title.style.width = "575px";
+    title.style.width = "400px";
     button1.innerHTML = "Continue";
     button2.innerHTML = "Go back";
     button3.innerHTML = "";
@@ -428,39 +476,134 @@ function treasure() {
     invItem.style.marginLeft = "0px";
     invItem.style.width = "100px";
     invItem.style.height = "280px";
+    button1.style.width = "100px";
     button2.style.width = "100px";
     button3.style.width = "100px";
 
-    button1.onclick = escape1;
+    button1.onclick = deadend2;
     button2.onclick = lvl6;
 
-    if (inventoryItem['explosives'] == true) {
-        invItem.onclick = carryExplosives;
-    }
-    else if (inventoryItem['explosives'] == false) {
-        invItem.onclick = statue;
+    invItem.onclick = functStatue;
+
+    if (inventoryItem['deadend2skip'] == true) {
+        button1.onclick = wayout;
     }
 }
-function escape1() {
+function treasurenoItem() {
     container.style.backgroundImage = "url('image/treasureroom.jpg')";
-    title.innerHTML = "You won.";
+    title.innerHTML = "You are back in the treasure room.";
 
     button1.style.display = "";
     button2.style.display = "";
     button3.style.display = "none";
     button4.style.display = "none";
-    title.style.width = "575px";
+    title.style.width = "700px";
     button1.innerHTML = "Continue";
     button2.innerHTML = "Go back";
     button3.innerHTML = "";
     button4.innerHTML = "";
     invItem.style.display = "none";
-    invItem.style.marginTop = "0px";
-    invItem.style.marginRight = "0px";
-    invItem.style.marginLeft = "0px";
+    button1.style.width = "100px";
+    button2.style.width = "100px";
+    button3.style.width = "100px";
 
-    button1.onclick = snakedeath;
     button2.onclick = lvl6;
+
+    if (inventoryItem['deadend2skip'] == true) {
+        button1.onclick = wayout;
+    }
+}
+function deadend2() {
+    container.style.backgroundImage = "url('image/deadend2.jpg')";
+    title.innerHTML = "You came across yet another dead end.";
+
+    button1.style.display = "";
+    button2.style.display = "";
+    button3.style.display = "none";
+    button4.style.display = "none";
+    title.style.width = "540px";
+    invItem.style.display = "none";
+    button1.innerHTML = "";
+    button2.innerHTML = "";
+    button3.innerHTML = "";
+    button4.innerHTML = "";
+
+    if (inventoryItem['explosives'] == true) {
+        button1.onclick = wayout;
+        button1.style.display = "";
+        button1.style.width = "125px";
+        button1.innerHTML = "Use explosives";
+    }
+    else if (inventoryItem['explosives'] == false) {
+        button1.onclick = wayout;
+        button1.style.display = "none";
+        button1.style.width = "125px";
+        button1.innerHTML = "Use explosives";
+    }
+    if (inventoryItem['statue'] == true) {
+        button2.onclick = treasurenoItem;
+        button2.style.display = "";
+        button2.innerHTML = "Go back";
+    }
+    else if (inventoryItem['statue'] == false) {
+        button2.onclick = treasure;
+        button2.style.display = "";
+        button2.innerHTML = "Go back";
+    }
+}
+function wayout() {
+    container.style.backgroundImage = "url('image/stairs.jpg')";
+    title.innerHTML = "You found some stairs.";
+
+    button1.style.display = "";
+    button2.style.display = "";
+    button3.style.display = "none";
+    button4.style.display = "none";
+    title.style.width = "340px";
+    button1.style.width = "100px";
+    invItem.style.display = "none";
+    button1.innerHTML = "Continue";
+    button2.innerHTML = "Go back";
+    button3.innerHTML = "";
+    button4.innerHTML = "";
+    deadend2skip();
+    falseExplosives();
+
+    button1.onclick = finish;
+
+    if (inventoryItem['statue'] == true) {
+        button2.onclick = treasurenoItem;
+        button2.style.display = "";
+    }
+    else if (inventoryItem['statue'] == false) {
+        button2.onclick = treasure;
+        button2.style.display = "";
+    }
+}
+function finish() {
+    container.style.backgroundImage = "url('image/finished.jpg')";
+    title.innerHTML = "You won.";
+
+    desc.style.display = "";
+    button1.style.display = "none";
+    button2.style.display = "none";
+    button3.style.display = "none";
+    button4.style.display = "none";
+    invItem.style.display = "none";
+    title.style.width = "150px";    
+    button1.innerHTML = "";
+    button2.innerHTML = "";
+    button3.innerHTML = "";
+    button4.innerHTML = "";
+
+    if (inventoryItem['statue'] == true) {
+        desc.innerHTML = "You grabbed the treasure.";
+        desc.style.backgroundColor = "green";
+        desc.style.width = "180px";
+    }
+    else if (inventoryItem['statue'] == false) {
+        desc.innerHTML = "You did not loot the treasure.";
+    }
 }
 function sanddeath() {
     container.style.backgroundImage = "url('image/sanddeath.jpg')";
@@ -595,6 +738,10 @@ function functExplosives() {
     inventoryItem["explosives"]=true;
     invItem.style.display = "none";
 }
+function falseExplosives() {
+    inventoryItem["explosives"]=false;
+}
+
 function functScroll() {
     inventoryItem["scroll"]=true;
     invItem.style.display = "none";
@@ -608,8 +755,11 @@ function functStatue() {
     invItem.style.display = "none";
 }
 function carryExplosives() {
-    alert("You are already carrying explosives.")
+    alert("You are carrying explosives already.")
 }
 function carryStatue() {
-    alert("You are already carrying a statue.")
+    alert("You are carrying a statue already.")
+}
+function deadend2skip() {
+    inventoryItem["deadend2skip"]=true;
 }
